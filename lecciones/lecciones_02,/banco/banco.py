f5ceb291a72249399012f19a1c53e5c01ced610b
cristian alejandro perez <mandymoorejcpaz@gmail.com>
2025-07-21 00:17:12 +0000

    saludo

--- lecciones/lecciones_02,/banco/banco.py
+++ lecciones/lecciones_02,/banco/banco.py
@@ -1,1 +1,10 @@
-saludo=input(" pide al usuario un saludo :")
+saludo=input(" pide al usuario un saludo :").lower().replace(" ","")
+if  saludo=="hola":
+    print("$ 0 ")
+elif saludo[0]=="h" or not "hola":
+    print("devuelve $20 ")
+else:
+    print("devuelve $100")
+#se debe consultar para tener una seguridad de la logica 
+
+
