4b1d892c339ea9062a0dd4e29064b4e4a1473812
cristian alejandro perez <mandymoorejcpaz@gmail.com>
2025-08-03 02:13:16 +0000

    coca

---
+++ lecciones/lecciones_03/maquina_coca_cola/maquina_coca_cola.py
@@ -0,0 +1,7 @@
+coca=50
+while True:
+    monedad=int(input("por favor ingrese monedad "))
+    resta_global=coca-monedad
+    if monedad==50:
+        print("precio justo se despacha botella de coca cola")
+#sigo mañana jajaja 
