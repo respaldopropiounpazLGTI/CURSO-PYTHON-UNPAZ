3a2f111ce050dfc7c3f025aeec4a1d5085e03720
cristian alejandro perez <mandymoorejcpaz@gmail.com>
2025-08-06 19:42:50 +0000

    maquina de coca cola

--- lecciones/lecciones_03/maquina_coca_cola/maquina_coca_cola.py
+++ lecciones/lecciones_03/maquina_coca_cola/maquina_coca_cola.py
@@ -1,7 +1,41 @@
 coca=50
+#debo `pregunta!`
 while True:
-    monedad=int(input("por favor ingrese monedad "))
-    resta_global=coca-monedad
-    if monedad==50:
-        print("precio justo se despacha botella de coca cola")
-#sigo mañana jajaja 
+    try:
+        print(f"cantidad adeudada {coca}")
+        moneda=int(input("ingrese moneda : "))
+        coca-=moneda
+        #print(f"cantidad adeudada {coca}")
+    except ValueError:
+        print(" solo numeros enteros !!!")
+        continue
+    #aca hice un contador y bajando el precio no es necesartio el if /elif pero lo pongo abajo y despues preguntp al rpofesor 
+    if moneda==50 or coca==0:
+        print("monto justo gracias por su compra ")
+        break
+    if moneda==25:
+        continue
+    elif moneda==10:
+        continue
+    elif moneda==5:
+        continue
+    else:
+        print("S")
+        break #fui por la parte de un contador en esete caso descuenta los if /elif no son necesario pero entiendo creo que es por las ramificaciones 
+    
+    
+   
+        
+    
+        
+        
+        
+    
+    
+    
+
+
+        
+        
+        
+    
